068adda092979101ea4273963c56d9edd15b541a
Asier <asieriko@gmail.com>
2014-11-07 22:46:06 +0100

    Refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -6,33 +7,41 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # She notices the page title and header mention students manager
-        assert 'Students' in self.browser.title, "Browser title was " + self.browser.title
+        # Edith headmaster has heard about a cool new online student management
+        # She goes to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention student manager
+        self.assertIn('Students Manager', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Students Manager', header_text)
 
         # She is invited to enter a student straight away
+        inputbox = self.browser.find_element_by_id('id_new_student')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a student'
+        )
 
-        # She types "Piotr ugrumov" into a text box
+        # She types "Piotr Ugrumov" into a text box
+        inputbox.send_keys('Piotr Ugrumov')
 
         # When she hits enter, the page updates, and now the page lists
-        # "Piotr Ugrumov" as a student
+        # "Piotr Ugrumov" as an item in a student list table
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'Piotr Ugrumov' for row in rows)
+        )
 
         # There is still a text box inviting her to add another student. She
-        # enters "Eddy Seigneur"
-
-        # The page updates again, and now shows both students on her list
-
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-
-        # She visits that URL - her students still there.
-
-        # Satisfied, she goes back to sleep
-
-if __name__ == '__main__':
-    unittest.main()
+        # enters "Eddie Seigneur"
+        self.fail('Finish the test!')
