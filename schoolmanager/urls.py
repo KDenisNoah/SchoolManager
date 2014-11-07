af9baa5748b32250402144846155c4b66459ff2d
Asier <asieriko@gmail.com>
2014-11-07 22:26:42 +0100

    First unit test and url mapping, dummy view

--- schoolmanager/urls.py
+++ schoolmanager/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'schoolmanager.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'students_manager.views.home_page', name='home'),
+    #url(r'^admin/', include(admin.site.urls)),
 )
